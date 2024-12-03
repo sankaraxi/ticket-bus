974cc0df13b0d5b8e09df7dd32395bc01b3778f6
sankaraxi <sankarfrompalani@gmail.com>
2024-12-03 21:04:29 +0530

    initialized with frontend requirements

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import { createBrowserRouter, Outlet } from "react-router-dom"
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./components/Home"
+import Booking from "./components/Booking"
+import BusList from "./components/BusList"
+
+
+const App = () => {
+    return (
+        <div className="">
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
+    )
+  }
+
+export const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children:[
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/booking",
+        element: <Booking />,
+      },
+      {
+        path: "/buses",
+        element: <BusList />,
+      },
+    ] 
+  }
+])
