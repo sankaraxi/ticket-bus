e7878253b9e318c637c7453b1e4d62ab6abc1257
sankaraxi <sankarfrompalani@gmail.com>
2024-12-04 00:00:59 +0530

    Enhance BusList component with improved styling, add location icon, and implement booking button; update global styles for better readability

--- frontend/src/components/BusList.js
+++ frontend/src/components/BusList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { CDN_URL } from '../utils/constants';
 
 import { IoLocationSharp } from "react-icons/io5";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const BusList = () => {
   const [buses, setBuses] = useState([]);
@@ -37,7 +38,7 @@ const BusList = () => {
   if (error) return <p>{error}</p>;
 
   return (
-    <div className=' mx-4 my-6 sm:mx-56  sm:my-9'>
+    <div className=' mx-4 my-6 sm:mx-72'>
       <div className="flex flex-col justify-center items-center">
           <h1 className="text-center font-palanquin font-bold text-3xl sm:text-4xl">Available Buses</h1>
           <div className="flex justify-center items-center mt-1 text-md sm:text-xl">
@@ -55,37 +56,41 @@ const BusList = () => {
         <>
           {buses.map((bus) => (
            <div
-           className=" relative  bg-slate-100 rounded-lg p-3 my-3 hover:bg-slate-200 cursor-pointer" 
+           className=" relative  bg-slate-300 rounded-lg p-3 my-3 hover:bg-slate-200 cursor-pointer" 
            key={bus?._id}
        >
            <div className="flex items-center flex-row relative p-2"> 
-               <img className="w-36 h-36 sm:w-3/12 sm:h-56 object-cover object-center rounded-lg"
-                   src={CDN_URL + bus?.image_id}
-                   alt="food-item"
-               />
+              
                 
 
                <div className="sm:w-9/12 flex flex-col m-4 p-4 justify-center">
-                   <h2 className="text-md sm:text-3xl font-poppins font-semibold">{bus?.name}</h2>
-                   <h4 className="text-md sm:text-lg font-palanquin ">{'\u20B9'} {bus?.price}</h4>
-                   <h4 className="text-md sm:text-lg font-palanquin "> {bus?.destination}</h4>
-                    <h4 className="text-md sm:text-lg font-palanquin "> {bus?.departureTime}</h4>
-                    <h4 className="text-md sm:text-lg font-palanquin "> {bus?.arrivalTime}</h4>
+                   <h2 className="text-md sm:text-6xl font-poppins pb-4 font-semibold">{bus?.name}</h2>
+                   <h4 className="flex gap-2 items-center text-md sm:text-3xl pb-4 font-palanquin "><span><FaLocationArrow /></span> <span className='font-bold'>{bus?.destination}</span></h4>
+                  
+                   
+                    <h4 className="text-md sm:text-xl font-palanquin "><span className='font-semibold'>Departure Time:</span> <span className='font-bold'>{bus?.departureTime}</span></h4>
+                    <h4 className="text-md sm:text-xl pb-4 font-palanquin "><span className='font-semibold'>Arrival Time:</span> <span className='font-bold'>{bus?.arrivalTime}</span></h4>
+                    <h4 className="text-md sm:text-3xl font-extrabold font-palanquin ">{'\u20B9'} {bus?.price}</h4>
                </div>
+
+               <img className="w-44 h-44 sm:w-3/12 sm:h-56 object-cover object-center rounded-md"
+                   src={CDN_URL + bus?.image_id}
+                   alt="bus-item"
+               />
 
              
            </div>
              
-           {/* <button 
-               className="absolute left-1 bottom-3 border border-solid border-transparent bg-orange-100 text-black p-2 ml-2 rounded-lg" key={item.card.info.id}
-               onClick={() => handleAddItems(item)}
-               > */}
-                   {/* <div className='' > */}
-                       {/* <h4 className='text-sm sm:text-lg'> */}
-                           {/* Add <span> <FontAwesomeIcon icon={faPlus} /></span> */}
-                       {/* </h4> */}
-                   {/* </div>     */}
-           {/* </button>   */}
+           <button 
+               className="absolute right-24 bottom-8 border border-solid border-transparent bg-red-500 text-white p-2 ml-2 rounded-md" key={bus?._id}
+              //  onClick={() => handleAddItems(item)}
+               >
+                   <div className='' >
+                       <h4 className='text-sm sm:text-lg'>
+                           Book Now
+                       </h4>
+                   </div>    
+           </button>  
            
        </div>  
           ))}
