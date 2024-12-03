42b43c0a29e24fbe3967b1b2018d1bffa8115865
sankaraxi <sankarfrompalani@gmail.com>
2024-12-03 21:19:32 +0530

    added header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import LOGO from '../assets/LOGO.png'
+import Nav from './Nav'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
   return (
-    <div>Header</div>
+    <div className='fixed w-full z-30 xl:py-5'>
+        <div className='z-50 xl:mx-0 px-4 xl:px-10 bg-white flex items-center justify-between bg-opacity-85 shadow-md'>
+            <Link to='/'>
+                <img 
+                    src={LOGO}
+                    className='w-g0 xl:w-64'
+                    alt='logo'
+                />
+            </Link>
+            <div className='flex items-center gap-2'>
+                <div className='hidden xl:flex'>
+                    <Nav />
+                </div>
+            </div>
+        </div>
+    </div>
+    
   )
 }
 
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+
+
+
+  return (
+    <div className='flex gap-4 text-lg'>
+        <Link to='/'>
+            <h1 className='cursor-pointer text-black hover:text-gray-400 font-semibold'>Home</h1>
+        </Link>
+        <Link to='/stats'>
+            <h1 className='cursor-pointer text-black hover:text-gray-400 font-semibold'>Stats</h1>
+        </Link>
+        <Link to='/about'>
+            <h1 className='cursor-pointer text-black hover:text-gray-400 font-semibold'>About</h1>
+        </Link>
+
+        
+    </div>
+  )
+}
+
+export default Nav
